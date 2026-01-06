1c3aad689523e6c6925beed071eecb33ab493e48
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2026-01-06 17:40:03 -0500

    build: Add JWT dependencies and secure configuration setup
    - Adds JJWT libraries to pom.xml (API, Impl, Jackson).
    - Configures secrets.properties for sensitive key storage.
    - Updates .gitignore to exlude secrets.properties.
    - Configures application.properties to import local secrets.
    - Scaffolds JwtUtils class with secret injection and token generation logic.

---
+++ src/main/java/com/rsandoval/ecommerce_api/security/JwtUtils.java
@@ -0,0 +1,48 @@
+package com.rsandoval.ecommerce_api.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration:86400000}")
+    private long jwtExpiration;
+
+    // 1. Generate Token
+    public String generateToken(String username) {
+        return createToken(new HashMap<>(), username);
+    }
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // 2. Validate Token
+
+    // 3. Extract Username
+
+    // --- HELPER METHODS ---
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}

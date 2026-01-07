8706d03f77246dd7665aa01f0613b637226507c2
rafaelSandovalR <102534410+rafaelSandovalR@users.noreply.github.com>
2026-01-07 13:37:35 -0500

    feat: Implement core JWT logic and configuration
    - Completes JwtUtils class with token validation and claim extraction methods.
    - Externalizes `jwt.expiration` to application.properties for better configurability.
    - Adds generic helper method for flexible claim retrieval.

--- src/main/java/com/rsandoval/ecommerce_api/security/JwtUtils.java
+++ src/main/java/com/rsandoval/ecommerce_api/security/JwtUtils.java
@@ -1,16 +1,19 @@
 package com.rsandoval.ecommerce_api.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtils {
@@ -18,6 +21,8 @@ public class JwtUtils {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    // Value is first pulled from application.properties first
+    // If the file is missing the line, it defaults to this value
     @Value("${jwt.expiration:86400000}")
     private long jwtExpiration;
 
@@ -36,8 +41,15 @@ public class JwtUtils {
     }
 
     // 2. Validate Token
+     public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+     }
 
     // 3. Extract Username
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
     // --- HELPER METHODS ---
     private Key getSignInKey() {
@@ -45,4 +57,24 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
